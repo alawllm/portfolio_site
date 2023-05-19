f3b7bc2d7340fc80f4f64623abff5d52e0922f94
alawllm <alicja.willam@gmail.com>
2023-05-19 10:13:06 +0200

    ignore commit trying to deploy

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,11 +1,6 @@
-import { Link } from 'react-scroll';
 import './footer.styles.scss'
 
 const Footer = () => {
-    // const [click, setClick] = useState(false)
-    // const handleClick = () => setClick(!click)
-
-    // const closeMenu = () => setClick(false)
     return (
         <div className='footer'>
             <p>This is the footer</p>
